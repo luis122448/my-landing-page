8fad029736eee9cfbbf33bedd2ce27265b422781
luis122448 <luis122448@gmail.com>
2023-12-04 22:55:53 -0500

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,6 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   site: 'https://luis122448.github.io',
-  base: '/my-landing-page',
+  base: '/',
   integrations: [tailwind()]
 });
